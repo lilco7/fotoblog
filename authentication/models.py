a561afaa7fbe18346f7167cbf3bb612677491dbb
Pape221Bop <papebop21@gmail.com>
2024-03-04 01:15:58 +0100

    Mise en place du modèle User d'authentification et application des migrations initiales

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,22 @@
+#Importation des modules 
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+#Définition du modèle User personnalisèe
+
+class User(AbstractUser):
+    #Définition des constantes de roles pour diférencier nos types d'utilisateurs
+
+    CREATOR = 'CREATOR'
+    SUBCRIBER = 'SUBCRIBER'
+
+    ROLES_CHOICES = (
+        (CREATOR, 'créateur'),
+        (SUBCRIBER, 'abonné'),
+    )
+
+    #Ajout des champs suplémentaires photo_profile et role
+
+    photo_profile = models.ImageField(verbose_name=('photo de profile'))
+    role = models.CharField(max_length=30, choices=ROLES_CHOICES, verbose_name=('role de l\'utilisateur'))
+ 
